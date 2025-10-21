ba6e9fc731448fe1b4c2d8fd34028fdf5c7e2686
ThePlayer453403 <3840728871@qq.com>
2025-10-21 20:21:38 +0800

    Change mod_id, Add first item

---
+++ src/main/java/com/github/elementcraft/item/ModItems.java
@@ -0,0 +1,14 @@
+package com.github.elementcraft.item;
+
+import com.github.elementcraft.ElementCraft;
+import com.github.elementcraft.item.custom.OriginalResin;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ElementCraft.MOD_ID);
+    public static final DeferredItem<Item> ORIGINAL_RESIN = ITEMS.register("original_resin", () -> new OriginalResin(new Item.Properties()));
+    public static void register(IEventBus modEventBus){ITEMS.register(modEventBus);}
+}
---
+++ src/main/java/com/github/elementcraft/item/custom/OriginalResin.java
@@ -0,0 +1,9 @@
+package com.github.elementcraft.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class OriginalResin extends Item {
+    public OriginalResin(Properties properties) {
+        super(properties);
+    }
+}
