16621a5ac5a6eee231591fea0b5b0f95cd390670
ThePlayer453403 <3840728871@qq.com>
2025-10-23 20:35:19 +0800

    修复Bug

--- src/main/java/com/github/mineastra/MineastraWonderland.java
+++ src/main/java/com/github/mineastra/MineastraWonderland.java
@@ -11,15 +11,12 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(MineastraWonderland.MOD_ID)
 public class MineastraWonderland {
     public static final String MOD_ID = "mineastra";
     public static final Logger LOGGER = LogUtils.getLogger();
     public MineastraWonderland(IEventBus modEventBus, ModContainer modContainer) {
-        NeoForge.EVENT_BUS.register(this);
-
         ModItems.register(modEventBus);
         ModComponents.register(modEventBus);
         ModCreativeModeTabs.register(modEventBus);
